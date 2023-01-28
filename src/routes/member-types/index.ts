bacf6bfb564604cb60ffac9de917b33021042fc4
Pavel Kizhlo <pavelkizh@gmail.com>
2023-01-28 17:42:37 +0300

    feat: implement member-type endpoints

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -8,7 +8,9 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 ): Promise<void> => {
   fastify.get('/', async function (request, reply): Promise<
     MemberTypeEntity[]
-  > {});
+  > {
+    return await fastify.db.memberTypes.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +19,16 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const memberType = await fastify.db.memberTypes.findOne({
+        key: 'id',
+        equals: request.params.id,
+      });
+      if (!memberType) {
+        throw reply.notFound('No member types with such id');
+      }
+      return memberType;
+    }
   );
 
   fastify.patch(
@@ -28,7 +39,16 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      if (!Object.keys(request.body).length) {
+        throw reply.badRequest('Add fields you want to update');
+      }
+      const updatedMemberType = await fastify.db.memberTypes.change(
+        request.params.id,
+        request.body
+      );
+      return updatedMemberType;
+    }
   );
 };
 

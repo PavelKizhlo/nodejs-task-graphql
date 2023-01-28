bae462a11597822b1f03bb13d9e379e9b6579f68
Pavel Kizhlo <pavelkizh@gmail.com>
2023-01-28 17:24:08 +0300

    feat: implement post endpoints

--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -2,11 +2,14 @@ import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-sc
 import { idParamSchema } from '../../utils/reusedSchemas';
 import { createPostBodySchema, changePostBodySchema } from './schema';
 import type { PostEntity } from '../../utils/DB/entities/DBPosts';
+import { NoRequiredEntity } from '../../utils/DB/errors/NoRequireEntity.error';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<PostEntity[]> {});
+  fastify.get('/', async function (request, reply): Promise<PostEntity[]> {
+    return await fastify.db.posts.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -15,7 +18,16 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity> {
+      const post = await fastify.db.posts.findOne({
+        key: 'id',
+        equals: request.params.id,
+      });
+      if (!post) {
+        throw reply.notFound('No posts with such id');
+      }
+      return post;
+    }
   );
 
   fastify.post(
@@ -25,7 +37,10 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: createPostBodySchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity> {
+      const newPost = await fastify.db.posts.create(request.body);
+      return newPost;
+    }
   );
 
   fastify.delete(
@@ -35,7 +50,16 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity> {
+      try {
+        const post = await fastify.db.posts.delete(request.params.id);
+        return post;
+      } catch (err) {
+        throw err instanceof NoRequiredEntity
+          ? reply.badRequest('No posts with such id')
+          : err;
+      }
+    }
   );
 
   fastify.patch(
@@ -46,7 +70,16 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity> {
+      if (!Object.keys(request.body).length) {
+        throw reply.badRequest('Add fields you want to update');
+      }
+      const updatedPost = await fastify.db.posts.change(
+        request.params.id,
+        request.body
+      );
+      return updatedPost;
+    }
   );
 };
 

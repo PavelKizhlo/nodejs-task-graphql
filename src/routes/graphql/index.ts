5e71e0a31bf3897f4ba5712ffffaeacdb58b3fe8
Pavel Kizhlo <pavelkizh@gmail.com>
2023-01-31 01:51:30 +0300

    feat: limit the complexity of the graphql queries by their depth

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { graphql } from 'graphql';
+import { graphql, validate, parse } from 'graphql';
+import * as depthLimit from 'graphql-depth-limit';
 import { graphqlBodySchema } from './schema';
 import schema from './graphql-schema/schema';
 
@@ -14,10 +15,18 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       },
     },
     async function (request, reply) {
+      const source = request.body.query!;
+      const documentNode = parse(source);
+      const validationRules = [depthLimit(5)];
+
+      const depthErrors = validate(schema, documentNode, validationRules);
+
+      if (depthErrors.length) reply.send({ data: null, errors: depthErrors });
+
       return await graphql({
         schema,
+        source,
         variableValues: request.body.variables,
-        source: String(request.body.query),
         contextValue: fastify,
       });
     }
